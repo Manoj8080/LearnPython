2c3d889708f917b329351c50d3abdc38d5eda610
Manoj8080 <72076479+Manoj8080@users.noreply.github.com>
2025-06-14 13:47:31 +0530

    Initial commit

---
+++ src/input_output.py
@@ -0,0 +1,16 @@
+#input()
+#accept user input
+
+user_input=input("enter : ")
+print("you entered : ",user_input)
+#input() fun returns the value as string
+print(type(user_input))
+
+#we can use int() and float() data types for input
+
+name=input("enter your name : ")
+print("hello ",name)
+age=int(input("enter your age"))
+print("your age is ",age)
+#int() can convert string to integer
+ 
---
+++ src/variables.py
@@ -0,0 +1,15 @@
+name = "manoj" #string
+#data type sting need to be in double or single quotes
+age = 21 #int
+height = 5.11 #float
+is_student = True #boolean
+
+print(name)
+print(age)
+print(height)
+print(is_student)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
